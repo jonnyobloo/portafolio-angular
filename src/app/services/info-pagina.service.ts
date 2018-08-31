b265003819bbb00bbc53da9c732fed9c14f95fd0
Luis Moreno <luis102128@outlook.com>
2018-08-30 17:40:04 -0700

    base de datos funcional y portafolio en proceso

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,28 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
-  constructor(private http: HttpClient) {
-    // console.log("servicio de infoPagina listo");
+  equipo: any[] = [];
 
-    // leer el archivo json
+  constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
+      .subscribe((resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
-    });
+      });
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-a6018.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+        this.equipo = resp;
+        // console.log(resp);
+      });
+  }
+
 }
